cd1c6fc6ff15e563a59997356ef648f230eed52c
bengrunfeld-wf <ben.grunfeld@webfilings.com>
2014-05-11 12:34:00 -0600

    Remove unused functions from auth.py

--- auth.py
+++ auth.py
@@ -5,50 +5,114 @@ Use the Google Flow workflow to walk through each stage of the OAuth 2.0
 process.
 """
 
+import json
 import sys
 import webapp2
 
 from urlparse import urlparse
 from urlparse import parse_qs
 
-from google.appengine.ext import db
+from google.appengine.api import urlfetch
 from google.appengine.api import users
 
 sys.path.append("lib")
 
-import httplib2
-
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.client import OAuth2WebServerFlow
-from oauth2client.appengine import CredentialsProperty
 from oauth2client.appengine import StorageByKeyName
 
-from model import CredentialsModel 
+from basehandler import BaseHandler
+from config import config
+from model import CredentialsModel
 
 
-def create_flow_object():
+GITHUB_ORGS_MEMBER_URL = 'https://api.github.com/user/orgs'
+GITHUB_API_URL = 'https://api.github.com'
+
+
+def fetch_url(url, method=urlfetch.GET, data=''):
+    """Send a HTTP request"""
+
+    result = urlfetch.fetch(url=url, method=method, payload=data,
+                            headers={'Access-Control-Allow-Origin': '*'})
+
+    return result.content
+
+
+def get_user_name():
+    """Get the name of the current user"""
+
+    url = '{}/user?access_token={}'.format(GITHUB_API_URL, get_access_token())
+
+    result = json.loads(fetch_url(url))
+
+    return result['login']
+
+
+def _create_flow_object():
     """Check if client_secrets.json is populated"""
 
-    # Enter your own scope and redirect_url here
-    scope = '' 
-    redirect_url = ''
-
     flow = flow_from_clientsecrets('./client_secrets.json',
-                                   scope,
-                                   redirect_url)
+                                   scope='user,repo',
+                                   redirect_uri='http://localhost:8080/code')
+
     return flow
 
-def get_auth_uri(flow):
+
+def _get_auth_uri(flow):
     """Provide the API keys and receive a temporary code in return"""
 
-    code = OAuth2WebServerFlow(client_id=flow.client_id,
-                           client_secret=flow.client_secret,
-                           scope=flow.scope,
-                           redirect_uri=flow.redirect_uri)
+    OAuth2WebServerFlow(client_id=flow.client_id,
+                        client_secret=flow.client_secret,
+                        scope=flow.scope,
+                        redirect_uri=flow.redirect_uri)
 
     auth_uri = flow.step1_get_authorize_url()
 
     return auth_uri
+
+
+def get_access_token():
+    """Used to check app activation and append token to urls"""
+
+    # Check if access token is in storage
+    storage = StorageByKeyName(CredentialsModel, 'token', 'credentials')
+
+    credentials = storage.get()
+
+    if not credentials:
+        return False
+
+    return credentials.access_token
+
+
+def _delete_access_token():
+    """Delete access token from storage"""
+
+    storage = StorageByKeyName(CredentialsModel, 'token', 'credentials')
+    storage.delete()
+
+
+def _retrieve_code(url):
+    """Retrieve code from url"""
+
+    parsed_url = parse_qs(urlparse(url).query)
+    code = parsed_url['code'][0]
+    return code
+
+
+class DetectActivation(BaseHandler):
+    """Detect if the app is set up"""
+
+    def get(self):
+
+        if not get_access_token() or not self.session.get('logged_in'):
+            # App is not set up or user is not logged in
+            self.redirect('/auth')
+            return
+
+        # Everything checks out, send user to auth
+        self.redirect('/app')
 
 
 class AuthUser(webapp2.RequestHandler):
@@ -56,55 +120,84 @@ class AuthUser(webapp2.RequestHandler):
 
     def get(self):
 
-        # Create a flow object to begin Google flow workflow
-        flow = create_flow_object()
+        # If User doesn't have Webfilings account, bail
+        user = users.get_current_user()
+
+        if not user:
+            self.redirect(users.create_login_url(self.request.uri))
+            return
+
+        # Begin Google's flow workflow
+        flow = _create_flow_object()
 
         if not flow:
-            self.error(404)         
+            self.error(500)
+            return
 
         # Use the flow object to construct an authorization uri
-        auth_uri = str(get_auth_uri(flow)) 
-   
+        auth_uri = str(_get_auth_uri(flow))
+
         if not auth_uri:
-            self.error(404)
+            self.error(500)
+            return
 
+        # After Github auth's the user, it returns them to
+        # the authorization callback URL used in the apps' GH settings
         self.redirect(auth_uri)
-        return 
+        return
 
 
-class RetrieveCode(webapp2.RequestHandler):
+class RetrieveToken(BaseHandler):
     """Switch the temporary code for an access token"""
 
     def get(self):
 
+        if get_access_token():
+            # App is set up,send them to the front page
+            self.session['logged_in'] = True
+            self.redirect('/app')
+            return
+
         # Get the code out of the url
-        url = self.request.url 
-        parsed_url = parse_qs(urlparse(url).query)
-        code = parsed_url['code'][0]
-       
-        flow = create_flow_object()
+        code = _retrieve_code(self.request.url)
 
         # Create a Credentials object which will hold the access token
+        flow = _create_flow_object()
         credentials = flow.step2_exchange(code)
-        
-        # Store the credentials object in the datastore
-        storage = StorageByKeyName(CredentialsModel, 
-                                   'auth_token', 'credentials')        
 
+        # Store the access token, app is now activated
+        storage = StorageByKeyName(CredentialsModel, 'token', 'credentials')
         storage.put(credentials)
 
-        # EXAMPLE: to get the access token out of storage
-        creds = storage.get()
-        print(creds.access_token)
-        return
+        # User is logged in
+        self.session['logged_in'] = True
+
+        context = {
+            "access_token": credentials.access_token,
+            "username": get_user_name(),
+        }
+
+        # Tell user that the app is activated
+        self.render('login', context)
 
 
-config = {}
-config['webapp2_extras.sessions'] = {
-    'secret_key': ''  # use secret key
-}
+class Logout(BaseHandler):
+    """Clear session variables and send to Github logout page"""
 
-application = webapp2.WSGIApplication([
-    ('/', AuthUser),
-    ('/code', RetrieveCode),
+    def get(self):
+
+        # Delete session vars
+        self.session.clear()
+
+        # Redirect to Github logout
+        self.redirect('https://github.com/logout')
+
+
+config = config()
+
+app = webapp2.WSGIApplication([
+    ('/', DetectActivation),
+    ('/auth', AuthUser),
+    ('/code', RetrieveToken),
+    ('/logout', Logout),
 ], config=config, debug=True)
